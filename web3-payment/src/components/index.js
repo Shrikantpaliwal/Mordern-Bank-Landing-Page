dbbcf38a993c4fcf0b7a3d65f7e753cf993600da
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-11-10 02:44:51 +0530

    Initial commit

---
+++ web3-payment/src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Billing from './Billing';
+import Clients from './Clients';
+import Button from './Button';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Testimonials from './Testimonials';
+import Footer from './Footer';
+import CTA from './CTA';
+import GetStarted from './GetStarted';
+import Stat from './Stats';
+import Feedback from './FeedbackCard';
+
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    Button,
+    Clients,
+    CardDeal,
+    Testimonials,
+    Footer,
+    CTA,
+    GetStarted,
+    Stat,
+    Feedback
+};

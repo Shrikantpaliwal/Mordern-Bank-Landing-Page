a0734afae10261019036df17e1515ef85460cc45
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-11-10 04:25:52 +0530

    Fixed App.jsx

--- web3-payment/src/App.jsx
+++ web3-payment/src/App.jsx
@@ -3,19 +3,21 @@ import styles from './style';
 import { Navbar, Hero, Stat, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => (
-  <div className="bg-dimBlue w-full overflow-hidden">
-    <div className={'${styles.paddingX} ${styles.flexcenter}'}>
-      <div className={'${styles.boxWidth'}>
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
-    <div className={'bg-primary ${styles.flexstart'}>
-      <div className={'${styles.boxWidth}'}>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
-    <div className={'bg-primary ${styles.paddingX} ${styles.flexstart'}>
-      <div className={'${styles.boxWidth}'}>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
         <Stat /> 
         <Business /> 
         <Billing /> 

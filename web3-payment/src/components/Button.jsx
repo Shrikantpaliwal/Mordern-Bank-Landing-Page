52218b1160c944225c38d81d31c7ec997c828659
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-11-15 01:12:10 +0530

    Button Comp & Business comp integrated using props

--- web3-payment/src/components/Button.jsx
+++ web3-payment/src/components/Button.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
 
-const Button = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
+const Button = ({ styles }) => (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary ${ styles}`}>Get Started</button>
+)
 
 export default Button

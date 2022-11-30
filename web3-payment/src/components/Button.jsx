e5c80a77c3a09937119145c291f1148bf399cc40
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-11-30 21:30:55 +0530

    Fixed Button Issue

--- web3-payment/src/components/Button.jsx
+++ web3-payment/src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 
 const Button = ({ styles }) => (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary ${ styles}`}>Get Started</button>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary rounded-[10px] ${ styles}`}>Get Started</button>
 )
 
 export default Button

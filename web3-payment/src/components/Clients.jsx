8f87c2337527cbee888e0ca4bdf69ecdc84c8382
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-11-30 22:34:21 +0530

    Client section complete

--- web3-payment/src/components/Clients.jsx
+++ web3-payment/src/components/Clients.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { clients } from '../constatants';
+import styles from "../style";
 
-const Clients = () => {
-    return (
-        <div>
-            
+const Clients = () => (
+    <section className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+            {clients.map((clients) => (
+                <div key={clients.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+                    <img src={clients.logo} alt="Client logo" className="sm:w-[192px] w-[100px] object-contain" />
+                </div>
+            ))}
         </div>
-    )
-}
+    </section>
+)
+
 
 export default Clients
